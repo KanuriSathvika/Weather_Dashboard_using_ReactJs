e5a1bb8e413aa4685e147dfe05dff5818fec1ecb
KanuriSathvika <kanurisathvika310@gmail.com>
2025-05-27 21:09:10 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Weather_Dashboard_using_ReactJs/',
   server: {
     port: 5174,
     open: true
